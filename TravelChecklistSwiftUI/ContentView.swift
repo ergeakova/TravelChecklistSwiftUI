a14ed74fab7403363180b885749c446b3750bdb0
ergeakova <ergeak@gmail.com>
2022-07-15 01:47:33 +0300

    Binding

--- TravelChecklistSwiftUI/ContentView.swift
+++ TravelChecklistSwiftUI/ContentView.swift
@@ -9,16 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     @State var characterName = "Luke SKYWALKER"
+    
     var body: some View {
         VStack{
             Text(characterName)
                 .font(.largeTitle)
                 .padding()
+            
             Button {
                 self.characterName = "Yoda"
             } label: {
                 Text("Change Character")
+                    .shadow(color: .white, radius: 0.5, x: 0, y: 0)
+                    .padding()
+                    .background(.pink)
+                    .cornerRadius(10)
             }
+            
+            TextField("", text: $characterName)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.blue)
+                .textFieldStyle(.roundedBorder)
+                .border(Color(.black), width: 2)
+                .padding()
         }
     }
 }
