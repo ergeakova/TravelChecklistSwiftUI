c490166fb0fc82f62d36a9f48f224b1feebabd57
ergeakova <ergeak@gmail.com>
2022-07-14 23:59:59 +0300

    Initial Commit

---
+++ TravelChecklistSwiftUI/TravelChecklistSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelChecklistSwiftUIApp.swift
+//  TravelChecklistSwiftUI
+//
+//  Created by Erge Gevher Akova on 14.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct TravelChecklistSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
